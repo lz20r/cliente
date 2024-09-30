931116c4eb6fc8a31fc962d60c0dede1eab37c71
lz20r <zhiyaonaiara@gmail.com>
2024-09-30 13:27:46 +0200

    Refactor html files and add number guessing game functionality

---
+++ Viernes 27 septiembre/TEMA2 Manejo de la sintásis del lenguaje/actividad/js/script.js
@@ -0,0 +1,7 @@
+ window.onload = function () {
+  let aleatorio = Math.floor(Math.random() * 10);
+  for (; aleatorio != parseInt(prompt("Introduce un número entre 0 y 10", ""));) {
+    
+  }
+  resultado.innerHTML = "¡Enhorabuena! Has acertado el número";
+};
