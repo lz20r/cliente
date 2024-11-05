eeb20e83a244069ca900a01ae907dc816f004f9e
lz20r <zhiyaonaiara@gmail.com>
2024-11-05 10:11:15 +0100

    Refactor CSS and JavaScript: Simplify styles and enhance highlighting functionality

--- Lunes 04 Noviembre/js/script.js
+++ Lunes 04 Noviembre/js/script.js
@@ -1,3 +1,23 @@
+/**
+ * @file script.js
+ * @description Script que permite buscar una palabra en un texto y resaltarla.
+ * @version 1.0
+ * @date 04/11/2022
+ * @author Naiara Zhiyao Lezameta Rodrigo
+ *
+ * documentacion de los metodos utilizados
+ * 1. addEventListener(): El método addEventListener() adjunta un controlador de eventos al elemento especificado.
+ * 2. preventDefault(): El método preventDefault() cancela el evento si es cancelable, lo que significa que la acción predeterminada que pertenece al evento no ocurrirá.
+ * 3. querySelector(): El método querySelector() devuelve el primer elemento que coincide con un selector de grupo de selectores CSS especificado en el documento.
+ * 4. replace(): El método replace() devuelve una nueva cadena con algunas o todas las coincidencias de un patrón, reemplazadas por un reemplazo.
+ * 5. innerHTML: La propiedad innerHTML establece o devuelve la sintaxis HTML o XML que describe los descendientes del elemento.
+ * 6. value: La propiedad value establece o devuelve el valor del atributo value de un campo de texto.
+ * 7. new RegExp(): El constructor RegExp crea un objeto de expresión regular para que coincida con el texto con un patrón.
+ * 8. gi: g: Encuentra todas las coincidencias en lugar de detenerse después de la primera coincidencia. i: Realiza una búsqueda sin distinción entre mayúsculas y minúsculas.
+ * 9. innerText: La propiedad innerText establece o devuelve el contenido de texto de los nodos y sus descendientes.
+ * 10. innerHTML: La propiedad innerHTML establece o devuelve la sintaxis HTML o XML que describe los descendientes del elemento.
+ */
+
 window.onload = function () {
   document.forms[0].addEventListener("submit", function (event) {
     event.preventDefault();
